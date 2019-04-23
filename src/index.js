549fb95948c74ae00868025258035e020be5f490
C <1129187719@qq.com>
2019-04-23 11:33:31 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 // @flow
-
+// 非固定大小表格
 export { default as VariableSizeGrid } from './VariableSizeGrid';
+// 非固定大小列表
 export { default as VariableSizeList } from './VariableSizeList';
+// 固定大小表格
 export { default as FixedSizeGrid } from './FixedSizeGrid';
+// 固定大小列表
 export { default as FixedSizeList } from './FixedSizeList';
 
 export { default as areEqual } from './areEqual';
